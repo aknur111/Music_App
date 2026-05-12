780168157c8150f8f2752ee8ad0668bdd99ff3c5
NoneNon9 <240565@astanait.edu.kz>
2026-05-12 23:52:50 +0500

    refactor

--- backend/playlist-service/internal/delivery/grpc/server.go
+++ backend/playlist-service/internal/delivery/grpc/server.go
@@ -109,17 +109,17 @@ func (s *Server) RemoveSongFromPlaylist(ctx context.Context, req *pb.RemoveSongR
 	return &pb.RemoveSongResponse{Success: true}, nil
 }
 
-//func (s *Server) AddCollaborator(ctx context.Context, req *pb.AddCollaboratorRequest) (*pb.AddCollaboratorResponse, error) {
-//	err := s.uc.AddCollaborator(ctx, req.PlaylistId, req.OwnerId, req.CollaboratorId)
-//	if err != nil {
-//		if err == usecase.ErrNotFound {
-//			return nil, status.Error(codes.NotFound, "playlist not found")
-//		}
-//		if err == usecase.ErrForbidden {
-//			return nil, status.Error(codes.PermissionDenied, "only the owner can add collaborators")
-//		}
-//		return nil, status.Error(codes.Internal, err.Error())
-//	}
-//
-//	return &pb.AddCollaboratorResponse{Success: true}, nil
-//}
+func (s *Server) AddCollaborator(ctx context.Context, req *pb.AddCollaboratorRequest) (*pb.AddCollaboratorResponse, error) {
+	err := s.uc.AddCollaborator(ctx, req.PlaylistId, req.OwnerId, req.CollaboratorId)
+	if err != nil {
+		if err == usecase.ErrNotFound {
+			return nil, status.Error(codes.NotFound, "playlist not found")
+		}
+		if err == usecase.ErrForbidden {
+			return nil, status.Error(codes.PermissionDenied, "only the owner can add collaborators")
+		}
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pb.AddCollaboratorResponse{Success: true}, nil
+}
